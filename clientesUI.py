0cd61d2764b0e72aa51912a4fa550f6dc11907e4
ocaiohen <caiohencontato@gmail.com>
2024-10-21 09:27:07 -0300

    Listar clientes e inserir clientes feitos no streamlit.

--- clientesUI.py
+++ clientesUI.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import pandas as pd
+import views
 class ClientesUI:
     @staticmethod
     def main():
@@ -17,12 +18,39 @@ class ClientesUI:
 
     @staticmethod        
     def listar():
-        pass
         # st.title("Listar Clientes")
-    
+        clientes = views.cliente_listar() 
+
+        if clientes:
+            clientes_data = {
+                "ID": [],
+                "Nome": [],
+                "Email": [],
+                "Fone": []
+            }
+
+            for c in clientes:
+                clientes_data["ID"].append(c.id)
+                clientes_data["Nome"].append(c.nome)
+                clientes_data["Email"].append(c.email)
+                clientes_data["Fone"].append(c.fone)
+            
+            clientes_df = pd.DataFrame(clientes_data)
+            st.dataframe(clientes_df)
+        else:
+            st.write("Não há clientes cadastrados")
+
     @staticmethod
     def inserir():
-        pass
+        nome = st.text_input("Nome: ")
+        email = st.text_input("Email: ")
+        fone = st.text_input("Fone: ")
+
+        if st.button("Adicionar cliente"):
+            if nome and email and fone:
+                views.cliente_inserir(nome, email, fone)
+            else:
+                st.error("Preencha todas as informações!")
         # st.title("Inserir Cliente")
     
     @staticmethod
